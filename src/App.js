978b2c1ecc209b870b513e81e84c141a74f155c0
sreeramvin <sreeramvin@gmail.com>
2020-05-31 12:27:21 +0530

    initial commit

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React ,{useState}from 'react';
+import Todo from './components/Todo';
+import './App.css';
+import Form from './components/Form';
+import FilterButton from './components/FilterButton'
+import {nanoid} from "nanoid";
+
+
+function App(props) {
+  const [tasks,setTasks]=useState(props.tasks)
+  function toggleTaskCompleted(id){
+    const updatedTasks = tasks.map(task =>{
+      if (id===task.id){
+        return{...task,completed:!task.completed} 
+      }
+      return task;
+    });
+    setTasks(updatedTasks);
+  }
+  function deleteTask(id){
+    const remainingTasks= tasks.filter(task => id!==task.id)
+    setTasks(remainingTasks);
+  }
+
+  const taskList = tasks.map(task =>( 
+    <Todo  
+    id={task.id} 
+    name={task.name} 
+    completed={task.completed}
+    key={task.id}
+    toggleTaskCompleted={toggleTaskCompleted}
+    deleteTask={deleteTask}
+    />
+    ));
+    function addTask(name) {
+      const newTask={ id :"task-"+nanoid(), name: name, completed:false    };
+      setTasks([...tasks, newTask]);
+    }
+    const tasksNoun = taskList.length !==1?'tasks':'task';
+    const headingText = `${taskList.length} ${tasksNoun} remaining`;
+    
+    
+    return (
+    <div className="todoapp stack-large">
+      <h1>TodoMatic</h1>
+      <Form addTask={addTask}/>
+      <div className="filters btn-group stack-exception">
+        <FilterButton />
+        <FilterButton />
+        <FilterButton />
+      </div>
+      <h2 id="list-heading">
+        {headingText}
+      </h2>
+      <ul
+        
+        className="todo-list stack-large stack-exception"
+        aria-labelledby="list-heading"
+      >
+        {taskList}
+
+      </ul>
+    </div>
+  );
+}
+export default App;
---
+++ src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const DATA = [
+  { id:'todo-0', name:'Eat', completed:true},
+  { id:'todo-1', name:'Sleep', completed:false},
+  { id:'todo=2', name:'Work', completed:false},
+]
+
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App tasks={DATA}/>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+
